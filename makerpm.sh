373579ac8efdd2b7b855cda9256d87b5427cc95b
E.J. Bevenour <ejb1123@users.noreply.github.com>
2016-09-15 13:37:41 -0400

    Update makerpm.sh

--- makerpm.sh
+++ makerpm.sh
@@ -1,3 +1,5 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 rpmdev-setuptree
 cp librevault.spec ~/rpmbuild/SPECS/
 cp memory.patch librevault-v0.1.18.9.tar.gz ~/rpmbuild/SOURCES/
