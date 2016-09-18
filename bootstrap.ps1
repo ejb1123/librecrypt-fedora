51bd98472f38bb467a28ed19e702bdea79a061ff
E.J. Bevenour <ejb1123@users.noreply.github.com>
2016-09-18 18:36:49 -0400

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -67,7 +67,7 @@ cmake -DBOOST_ROOT="C:\Libraries\boost_1_59_0"`
 function setupVSudio(){
 #Set environment variables for Visual Studio Command Prompt
 pushd "$env:VS140COMNTOOLS"
-cmd /c 'vsvars32.bat&set' |
+cmd /c 'vsvars32.bat`&set' |
 foreach {
   if ($_ -match "=") {
     $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
