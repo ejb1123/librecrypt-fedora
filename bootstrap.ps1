2b243229582689d32266d5d45e42923468784632
E.J. Bevenour <ejb1123@users.noreply.github.com>
2016-09-18 17:08:45 -0400

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -4,10 +4,10 @@ cd tmp
 Import-Module BitsTransfer
 if(!(Test-Path ".\jom.zip")) {
   Start-BitsTransfer -source 'http://download.qt.io/official_releases/jom/jom.zip' -Destination "$PSScriptRoot\tmp\jom.zip"
-  7z x jom.zip -ojom
+  & 7z x jom.zip -ojom
 }
 if(!(Test-Path ".\jom")) {
-  7z x jom.zip -ojom
+  & 7z x jom.zip -ojom
 }
 $env:Path += ";"+ $(Resolve-Path .\jom\)
 
@@ -24,10 +24,10 @@ jom install
 
 if(!(Test-Path ".\qt-everywhere-opensource-src-5.7.0.7z")) {
   Start-BitsTransfer -Priority Foreground -source "http://download.qt.io/archive/qt/5.7/5.7.0/single/qt-everywhere-opensource-src-5.7.0.7z" -Destination "$PSScriptRoot\tmp\qt-everywhere-opensource-src-5.7.0.7z"
-  7z x qt-everywhere-opensource-src-5.7.0.7z
+  & 7z x qt-everywhere-opensource-src-5.7.0.7z
 }
 if(!(Test-Path ".\qt-everywhere-opensource-src-5.7.0")) {
-  7z x qt-everywhere-opensource-src-5.7.0.7z
+  & 7z x qt-everywhere-opensource-src-5.7.0.7z
 }
 cd qt-everywhere-opensource-src-5.7.0
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
