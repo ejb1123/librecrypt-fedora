18e2b408c840a5b1826cf3640874bc0f76ca3e20
E.J. Bevenour <ejb1123@users.noreply.github.com>
2016-09-20 19:59:56 -0400

    Update makerpm.sh

--- makerpm.sh
+++ makerpm.sh
@@ -1,9 +1,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/..
+$version=$(git describe)
+export version=$(git describe)
 cd $DIR
 rpmdev-setuptree
-tar -czvf librevault-v0.1.18.11.tar.gz $DIR/../../librevault
+tar -czvf librevault-$version.tar.gz $DIR/../../librevault
 cp librevault.spec ~/rpmbuild/SPECS/
-cp memory.patch librevault-v0.1.18.11.tar.gz ~/rpmbuild/SOURCES/
+cp memory.patch librevault-$version.tar.gz ~/rpmbuild/SOURCES/
 echo -e "%_topdir %(echo $HOME)/rpmbuild\n%__make /usr/bin/make -j$(echo "$(nproc) + $(nproc)/2" | bc)" > ~/.rpmmacros
 cd ~/rpmbuild/SPECS/
 dnf copr enable -y ejb1123/protobuf
